f3ce318b26366afb34bf93b0f729096cf756df85
Tyler Auer <Tyler.Auer@gmail.com>
2021-09-09 22:43:35 -0600

    Add delete mutation

--- api/src/resolvers/task_resolver.ts
+++ api/src/resolvers/task_resolver.ts
@@ -1,4 +1,4 @@
-import { Arg, Field, InputType, Mutation, Query, Resolver } from 'type-graphql';
+import { Arg, Field, InputType, Mutation, ObjectType, Query, Resolver } from 'type-graphql';
 import { Task } from '../entity/Task';
 
 @InputType()
@@ -11,6 +11,14 @@ class TaskInput {
   due_date: Date;
 }
 
+@ObjectType()
+class ActionResult {
+  @Field()
+  success: boolean;
+  @Field()
+  message: string;
+}
+
 @Resolver(Task)
 export class TaskResolver {
   @Query(() => [Task], { nullable: true })
@@ -21,5 +29,22 @@ export class TaskResolver {
   @Mutation(() => Task)
   createTask(@Arg('input') input: TaskInput): Promise<Task> {
     return Task.create({ ...input }).save();
+  }
+
+  @Mutation(() => ActionResult)
+  async deleteTask(@Arg('id') id: string): Promise<ActionResult> {
+    const deleteResult = await Task.delete(id);
+
+    if (deleteResult.affected === 0) {
+      return {
+        success: false,
+        message: 'Task not found. Unable to delete task.',
+      };
+    }
+
+    return {
+      success: true,
+      message: 'Task deleted successfully',
+    };
   }
 }
