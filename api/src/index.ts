cb9a89de171d4d9a9820dd7f23fb0555cba14505
Tyler Auer <Tyler.Auer@gmail.com>
2021-10-09 21:43:34 -0600

    Good except connecting to PSQL

--- api/src/index.ts
+++ api/src/index.ts
@@ -8,7 +8,7 @@ import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-co
 import { CustomContextType } from './types';
 import constants from './constants';
 import redis from 'redis';
-import session from 'express-session';
+import expressSession from 'express-session';
 import connectRedis from 'connect-redis';
 require('dotenv').config();
 
@@ -16,8 +16,8 @@ main();
 
 async function main() {
   const app = express();
-  connectToPostgres();
-  addRedisMiddleware(app);
+  await connectToPostgres();
+  await addRedisMiddleware(app);
   startApolloServer(app);
 
   app.listen(constants.PORT, () => {
@@ -26,42 +26,71 @@ async function main() {
 }
 
 async function connectToPostgres() {
-  await createConnection({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'tylerauer',
-    password: 'postgres',
-    database: 'one-to-rule-them-all',
-    entities: entities,
-    synchronize: true,
-    logging: true,
-  }).catch((err) => {
-    console.log(err);
-  });
+  let retriesAllowed = 10;
+  const waitBetweenRetries = 5000;
+
+  while (retriesAllowed) {
+    try {
+      await createConnection({
+        type: 'postgres',
+        host: 'postgres://postgres:postgres@postgres:5432/one-to-rule-them-all',
+        port: 5432,
+        username: 'postgres',
+        password: 'postgres',
+        database: 'one-to-rule-them-all',
+        entities: entities,
+        synchronize: true,
+        logging: true,
+      });
+      console.log('Successfully connected to PostgreSQL');
+      break;
+    } catch (err) {
+      retriesAllowed--;
+      console.log(`Failed to connect to PostgreSQL ${retriesAllowed} retries left.`);
+      console.log(err);
+      await new Promise((resolve) => setTimeout(resolve, waitBetweenRetries));
+    }
+  }
 }
 
-function addRedisMiddleware(app: Express) {
-  const RedisStore = connectRedis(session);
-  const redisClient = redis.createClient();
-  app.use(
-    session({
-      name: 'qid',
-      store: new RedisStore({
-        client: redisClient,
-        ttl: constants.SESSION_TTL,
-      }),
-      cookie: {
-        maxAge: constants.TIME.YEAR * 2,
-        httpOnly: true,
-        sameSite: 'lax', // csrf
-        secure: process.env.NODE_ENV === 'production', // cookie only works in https
-      },
-      saveUninitialized: false,
-      secret: process.env.SESSION_SECRET as string,
-      resave: false,
-    })
-  );
+async function addRedisMiddleware(app: Express) {
+  let retriesAllowed = 10;
+  const waitBetweenRetries = 5000;
+
+  while (retriesAllowed) {
+    try {
+      const RedisStore = connectRedis(expressSession);
+      const redisClient = redis.createClient({
+        port: 6379,
+        host: 'localhost',
+      });
+      app.use(
+        expressSession({
+          name: 'qid',
+          store: new RedisStore({
+            client: redisClient,
+            ttl: constants.SESSION_TTL,
+          }),
+          cookie: {
+            maxAge: constants.TIME.YEAR * 2,
+            httpOnly: true,
+            sameSite: 'lax', // csrf
+            secure: process.env.NODE_ENV === 'production', // cookie only works in https
+          },
+          saveUninitialized: false,
+          secret: process.env.SESSION_SECRET as string,
+          resave: false,
+        })
+      );
+      console.log('Successfully connected to Redis');
+      break;
+    } catch (err) {
+      retriesAllowed--;
+      console.log(`Failed to connect to Redis ${retriesAllowed} retries left.`);
+      console.log(err);
+      await new Promise((resolve) => setTimeout(resolve, waitBetweenRetries));
+    }
+  }
 }
 
 async function startApolloServer(app: Express) {
